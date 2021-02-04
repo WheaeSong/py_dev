89ff3e3d197b9854886e1a8acb55e0f6990c6dee
WheaeSong <2587831276@qq.com>
2021-02-04 13:27:36 +0800

    02-04

--- MVC_Django框架/bj28/test1/booktest/urls.py
+++ MVC_Django框架/bj28/test1/booktest/urls.py
@@ -3,5 +3,7 @@ from booktest import views
 
 urlpatterns = [
         url(r'^index$',views.index),  # 建立/index和视图index之间的关系
-        url(r'^index2$',views.index2)
+        url(r'^index2$',views.index2),
+        url(r'^books$',views.books), # 显示图书信息
+        
         ]
--- MVC_Django框架/bj28/test1/booktest/views.py
+++ MVC_Django框架/bj28/test1/booktest/views.py
@@ -1,32 +1,39 @@
-from django.template import loader,RequestContext
+from django.template import loader, RequestContext
 from django.shortcuts import render
+from booktest.models  import BookInfo
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 # 1 定义视图函数: HttpRequest
 # 2 进行URL配置 http:127.0.0.1：8000/index，建立视图与url地址的对应关系
 
 
-
-def my_render(request,templates_path,context_dict={}):
-    #return HttpResponse('老铁，没毛病')    
+def my_render(request, templates_path, context_dict={}):
+    # return HttpResponse('老铁，没毛病')
     # 使用模板
     # 1 加载模板文件内容,模板对象
     temp = loader.get_template(templates_path)
     # 定义模板上下文
-    context= RequestContext(request,context_dict)
+    context = RequestContext(request, context_dict)
     # 3 模板渲染产生标准的html内容
     res_html = temp.render(context)
     # 返回给浏览器
     return HttpResponse(res_html)
 
 
-
 def index(request):
     # 进行处理，与M和T交互
-    return render(request,'booktest/index.html',{'content': "hello world"})
-        
+    return render(request, 'booktest/index.html', {'content': "hello world"})
 
 
 def index2(request):
     return HttpResponse('hello python')
+
+
+def show_books(request):
+    """显示图书信息"""
+    # 1. 通过M查找图书表中的数据
+    books = BookInfo.objects.all()
+    return render(request, 'booktest/show_books.html', {'books': books})
